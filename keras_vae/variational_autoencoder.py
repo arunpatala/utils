e392e83f408ff8def83755e6901bf50f997de379
Arun Patala <arun.patala@sankheki.com>
2018-02-14 15:31:07 +0530

    save plot of VAE sampling, keras vae mnist 0.11

--- keras_vae/variational_autoencoder.py
+++ keras_vae/variational_autoencoder.py
@@ -17,6 +17,8 @@ from keras import backend as K
 from keras import metrics
 from keras.datasets import mnist
 
+from keras.callbacks import ModelCheckpoint, LambdaCallback
+
 batch_size = 100
 original_dim = 784
 latent_dim = 2
@@ -67,21 +69,19 @@ x_test = x_test.astype('float32') / 255.
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
-vae.fit(x_train,
-        shuffle=True,
-        epochs=epochs,
-        batch_size=batch_size,
-        validation_data=(x_test, None))
 
-# build a model to project inputs on the latent space
-encoder = Model(x, z_mean)
+def plot_test():
+    # build a model to project inputs on the latent space
+    encoder = Model(x, z_mean)
 
-# display a 2D plot of the digit classes in the latent space
-x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
-plt.figure(figsize=(6, 6))
-plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test)
-plt.colorbar()
-plt.show()
+    # display a 2D plot of the digit classes in the latent space
+    x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
+    plt.figure(figsize=(6, 6))
+    plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test)
+    plt.colorbar()
+    plt.show()
+
+
 
 # build a digit generator that can sample from the learned distribution
 decoder_input = Input(shape=(latent_dim,))
@@ -89,23 +89,40 @@ _h_decoded = decoder_h(decoder_input)
 _x_decoded_mean = decoder_mean(_h_decoded)
 generator = Model(decoder_input, _x_decoded_mean)
 
-# display a 2D manifold of the digits
-n = 15  # figure with 15x15 digits
-digit_size = 28
-figure = np.zeros((digit_size * n, digit_size * n))
-# linearly spaced coordinates on the unit square were transformed through the inverse CDF (ppf) of the Gaussian
-# to produce values of the latent variables z, since the prior of the latent space is Gaussian
-grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
-grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
 
-for i, yi in enumerate(grid_x):
-    for j, xi in enumerate(grid_y):
-        z_sample = np.array([[xi, yi]])
-        x_decoded = generator.predict(z_sample)
-        digit = x_decoded[0].reshape(digit_size, digit_size)
-        figure[i * digit_size: (i + 1) * digit_size,
-               j * digit_size: (j + 1) * digit_size] = digit
+def plot_samples(epoch):
+    # display a 2D manifold of the digits
+    n = 15  # figure with 15x15 digits
+    digit_size = 28
+    figure = np.zeros((digit_size * n, digit_size * n))
+    # linearly spaced coordinates on the unit square were transformed through the inverse CDF (ppf) of the Gaussian
+    # to produce values of the latent variables z, since the prior of the latent space is Gaussian
+    grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
+    grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
 
-plt.figure(figsize=(10, 10))
-plt.imshow(figure, cmap='Greys_r')
-plt.show()
+    for i, yi in enumerate(grid_x):
+        for j, xi in enumerate(grid_y):
+            z_sample = np.array([[xi, yi]])
+            x_decoded = generator.predict(z_sample)
+            digit = x_decoded[0].reshape(digit_size, digit_size)
+            figure[i * digit_size: (i + 1) * digit_size,
+                   j * digit_size: (j + 1) * digit_size] = digit
+
+    plt.figure(figsize=(10, 10))
+    #plt.imshow(figure, cmap='Greys_r')
+    plt.imsave('models/samples{}.png'.format(epoch), figure, cmap='Greys_r')
+    #plt.show()
+
+lambda_callback = LambdaCallback(
+    on_epoch_end=lambda epoch, logs: plot_samples(epoch)
+)
+
+ckpt = ModelCheckpoint('models/vae.{epoch:02d}.ckpt.tgz')
+
+vae.fit(x_train,
+        shuffle=True,
+        epochs=epochs,
+        batch_size=batch_size,
+        validation_data=(x_test, None), 
+        callbacks=[ckpt, lambda_callback])
+
